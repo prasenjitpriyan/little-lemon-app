a9074f9c9bebdcb496478eedbb14c925a047e3f7
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-11-29 08:50:48 +0530

    Text input added

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 import FlatLists from '@/components/FlatList'
 import MenuItems from '@/components/MenuItems'
 import SectionLists from '@/components/SectionList'
+import FeedbackForm from '@/components/TextInput'
 import WelcomeScreen from '@/components/WelcomeScreen'
 import { View, StyleSheet } from 'react-native'
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <SectionLists />
+      <FeedbackForm />
     </View>
   )
 }
